ac58dacd49bb5af0aac9f81f1179df14079f0794
Alefie <Zander.fierenz@gmx.de>
2019-08-27 16:25:41 +0200

    Add files via upload

--- Trimp_structure.R
+++ Trimp_structure.R
@@ -3,7 +3,9 @@ setwd("C:/Users/Zander/Uni/R-Projekt")
 data <- readRDS("eightruns.rds")
 head(data[[1]])
 
-setClass("activity", representation(time="POSIXt", latitude="numeric",longitude="numeric",altitude="numeric",distance="numeric",heart_rate="numeric",speed="numeric",cadence="numeric"))
+setClass("activity", representation(time="POSIXt", latitude="numeric", longitude="numeric", altitude="numeric", distance="numeric", heart_rate="numeric", speed="numeric", cadence="numeric", duration="numeric"))
+setClass("athlete", representation(name="character", sex="character", HRest="numeric", HMax="numeric"))
+
 
 a <- new("activity", time=data[[1]]$time, latitude=data[[1]]$latitude, longitude=data[[1]]$longitude, altitude=data[[1]]$altitude, distance=data[[1]]$distance, heart_rate=data[[1]]$heart_rate, speed=data[[1]]$speed, cadence=data[[1]]$cadence)
 a@time
@@ -11,8 +13,19 @@ a@distance[1]
 
 # function initialize activity class with dist in km and speed in km/h
 toactivity <- function(df) {
-  activity <- new("activity", time=df$time,latitude=df$latitude, longitude=df$longitude, altitude=df$altitude, distance=df$distance/1000, heart_rate=df$heart_rate, speed=df$speed*3.6, cadence=df$cadence)
+  start <- c(as.POSIXct(df$time[1]), df$time[1:nrow(df)-1])
+  dur <- as.numeric(df$time-start)
+  activity <- new("activity", time=df$time,latitude=df$latitude, longitude=df$longitude, altitude=df$altitude, distance=df$distance/1000, heart_rate=df$heart_rate, speed=df$speed*3.6, cadence=df$cadence, duration=dur)
   return(activity)
 }   
 b <- toactivity(data[[1]])
 b
+
+dieter <- new("athlete", name="Dieter", sex="male", HRest=60, HMax=190)
+dieter
+
+#function for data of athlete
+toathlete <- function(name="Mustermann", restHR, maxHR, sex="female"){
+  athlete <- new("athlete", name=name, sex=sex, HRest=restHT, HMax= maxHR)
+  return(athlete)
+}
